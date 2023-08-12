011b1d5671c84870c0e12009fe11f8918538e322
saragabr1 <140217626+saragabr1@users.noreply.github.com>
2023-08-12 12:07:28 +0300

    Add files via upload

---
+++ .ino.ino
@@ -0,0 +1,13 @@
+int led[3]={3,4,5};
+int sens[3]={0,512,682};
+void setup(){for(int i=0;i<=2;i++){pinMode(led[i],OUTPUT);}}
+void loop()
+  {if(analogRead(A0)==sens[0]{digitalWrite(led[0],HIGH);}
+      
+          eles if(analogRead(A0)==sens[0]{digitalWrite(led[0],HIGH);}  
+         eles if(analogRead(A0)==sens[1]{digitalWrite(led[1],HIGH);}  
+          eles(analogRead(A0)==sens[2]{digitalWrite(led[2],HIGH);}  
+               else{for(int i=0;i<1;i++){digitalWrite(led[i],low;)}                                
+
+               }        
+                      
